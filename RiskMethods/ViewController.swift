ff4b36774813c8566d80bcb17ef11587d8dd167e
Marcin Jucha <jucham01@stepstone.com>
2021-05-16 11:40:46 +0200

    Initial Commit

---
+++ RiskMethods/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RiskMethods
+//
+//  Created by jucham01 on 16/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
